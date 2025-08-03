92115ff1745fccf81b4d84128d0b4c89086417a8
Svetlana Bilous <sbilous@edu.lapinamk.fi>
2025-08-03 20:39:11 +0300

    added https logic

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,31 @@ function App() {
   const [sentiment, setSentiment] = useState('');
 
   // Function to handle sentiment analysis (simulated with random choice)
-  const handleAnalyze = (text) => {
+  const handleAnalyze = async (text) => {
 
     // If input is empty or only spaces, clear result and return
     if (!text.trim()) {
       setSentiment('');
       return;
     }
+    try {
+      const response = await fetch("https://cloud-computing-backend-open-cloud-computing-task4-backend.2.rahtiapp.fi/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text: text })
+      });
 
-    // Simulated sentiment options
-    const options = ['Positive', 'Neutral', 'Negative'];
+      const data = await response.json();
 
-    // Pick a random result from the options
-    const randomResult = options[Math.floor(Math.random() * options.length)];
+      // Преобразуем результат с маленькой буквы в Capitalized (чтобы сохранить стиль интерфейса)
+      const formatted = data.class.charAt(0).toUpperCase() + data.class.slice(1);
+      setSentiment(formatted);
 
-    // Update sentiment state with the result
-    setSentiment(randomResult);
+    } catch (error) {
+      console.error("Analyze error:", error);
+      setSentiment("Error");
+    }
+
   };
 
   return (
